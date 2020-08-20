7d3d90db89192719c9ff0f6af79e1b60f6e5be44
Vampiricaura66 <benjamin.nicholson@my.jcu.edu.au>
2020-08-20 15:14:43 +1000

    add function to broken_score.py and a random number generator

--- cp1404practicals/Prac_03/broken_score.py
+++ cp1404practicals/Prac_03/broken_score.py
@@ -2,16 +2,27 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score >= 0:
-    print("Bad")
-else:
-    print("Invalid score")
 
+def main():
+    score = int(input("Enter score: "))
+    result = calculate_score(score)
+    print(result)
+    score = random.randint(0, 100)
+    print(score)
+    print(calculate_score(score))
+
+
+def calculate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
